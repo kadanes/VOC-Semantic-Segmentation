15b2118fd0f6e1e9a3d474b2baea2eff92eed005
Parth Tamane <ptamane3@gatech.edu>
2020-11-17 21:55:20 -0500

    added droput and batch norm

--- model/Naive.py
+++ model/Naive.py
@@ -13,20 +13,22 @@ class Naive(nn.Module):
         self.encoder = nn.Sequential(
             nn.Conv2d(3, 64, 3, stride=2, padding=1),
             nn.LeakyReLU(),
+            nn.BatchNorm2d(64),
+            nn.Dropout2d(),
+            # nn.MaxPool2d(2),
             nn.Conv2d(64, 128, 3, stride=2, padding=1),
             nn.LeakyReLU(),
+            nn.BatchNorm2d(128),
+            nn.Dropout2d(),
+            # nn.MaxPool2d(2),
             nn.Conv2d(128, 256, 3, stride=2, padding=1),
             nn.LeakyReLU(),
+            nn.BatchNorm2d(256),
+            nn.Dropout2d(),
+            # nn.MaxPool2d(2),
             nn.Conv2d(256, 512, 3, stride=2, padding=1),
         )
 
-        # self.conv1 = nn.Conv2d(3, 64, 3, stride=2, padding=1)
-        # self.relu1 = nn.ReLU()
-        # self.conv2 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
-        # self.relu2 = nn.ReLU()
-        # self.conv3 = nn.Conv2d(128, 256, 3, stride=2, padding=1)
-        # self.relu3 = nn.ReLU()
-        # self.conv4 = nn.Conv2d(256, 512, 3, stride=2, padding=1)
 
         self.decorder = nn.Sequential(
             nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1),
@@ -41,7 +43,19 @@ class Naive(nn.Module):
     # ReLU and Tanh cause output to be all 0
 
     def forward(self, x):
+        transpose = nn.ConvTranspose2d(512, 256, 3, stride=2, padding=1, output_padding=1),
+
         encoded = self.encoder(x)
+
+        pool = nn.Sequential(
+            nn.Conv2d(3, 64, 3, stride=2, padding=1),
+            nn.LeakyReLU(),
+            nn.MaxPool2d(2)
+        )
+
+        print("Pool:", pool(x).shape)
+
+
         decoded = self.decorder(encoded)
         return decoded
 
